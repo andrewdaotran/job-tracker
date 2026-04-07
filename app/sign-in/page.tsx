ee2dabaaa0ad41bb0f7ab2d85ec480795f27a067
Andrew Tran <andrewdaotran@gmail.com>
2026-04-07 00:56:12 -0700

    Finished authentication

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -11,9 +11,42 @@ import {
 } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { signIn } from '@/lib/auth/auth-client'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 export default function SignIn() {
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+
+	const [error, setError] = useState('')
+	const [loading, setLoading] = useState(false)
+
+	const router = useRouter()
+
+	async function handleSubmit(e: React.SubmitEvent) {
+		e.preventDefault()
+
+		setError('')
+		setLoading(true)
+
+		try {
+			const result = await signIn.email({
+				email,
+				password,
+			})
+			if (result.error) {
+				setError(result.error.message ?? 'Failed to sign in')
+			} else {
+				router.push('/dashboard')
+			}
+		} catch (err) {
+			setError('An unexpected error occured')
+		} finally {
+			setLoading(false)
+		}
+	}
 	return (
 		<div className='flex min-h-[cal(100vh-4rem)] items-center justify-center bg-white p-4'>
 			<Card className='w-full max-w-md border-gray-200 shadow-lg'>
@@ -25,8 +58,13 @@ export default function SignIn() {
 						Enter your credentials to access your account
 					</CardDescription>
 				</CardHeader>
-				<form className='space-y-4'>
+				<form className='space-y-4' onSubmit={handleSubmit}>
 					<CardContent className='space-y-4'>
+						{error && (
+							<div className='rounded-md bg-destructive/15 p-3 text-sm text-destructive'>
+								{error}
+							</div>
+						)}
 						<div className='space-y-2'>
 							<Label htmlFor='email' className='text-gray-700'>
 								Email
@@ -34,6 +72,8 @@ export default function SignIn() {
 							<Input
 								id='email'
 								type='email'
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
 								placeholder='johndoe123@example.com'
 								required
 								className='border-gray-300 focus:border-primary focus:ring-primary'
@@ -47,6 +87,8 @@ export default function SignIn() {
 								id='password'
 								type='text'
 								placeholder='John Doe'
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
 								minLength={8}
 								required
 								className='border-gray-300 focus:border-primary focus:ring-primary'
@@ -57,8 +99,9 @@ export default function SignIn() {
 						<Button
 							type='submit'
 							className='w-full bg-primary hover:bg-primary/90'
+							disabled={loading}
 						>
-							Sign In
+							{loading ? 'Signing in...' : 'Sign In'}
 						</Button>
 						<p className='text-center text-sm text-gray-600'>
 							Don't have an account?
