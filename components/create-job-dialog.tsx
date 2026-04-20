93a9122fef6fbd387fd6b0fcc824f5b9894b9725
Andrew Tran <andrewdaotran@gmail.com>
2026-04-20 16:59:27 -0700

    Add button to open dialog box to add a job application and skeleton of dialog box

--- components/create-job-dialog.tsx
+++ components/create-job-dialog.tsx
@@ -1,4 +1,15 @@
-import React from 'react'
+import { Plus } from 'lucide-react'
+import { Button } from './ui/button'
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from './ui/dialog'
+import { Input } from './ui/input'
+import { Label } from './ui/label'
 
 interface CreateJobApplicationDialogProps {
 	columnId: string
@@ -9,7 +20,44 @@ const CreateJobApplicationDialog = ({
 	columnId,
 	boardId,
 }: CreateJobApplicationDialogProps) => {
-	return <></>
+	return (
+		<Dialog>
+			<Button asChild variant='outline'>
+				<DialogTrigger>
+					<Plus />
+					Add Job
+				</DialogTrigger>
+			</Button>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>Add Job Application</DialogTitle>
+					<DialogDescription>Track a new job application</DialogDescription>
+				</DialogHeader>
+				<form>
+					<div>
+						<div>
+							<div>
+								<Label> Company *</Label>
+								<Input id='company' required />
+							</div>
+							<div>
+								<Label> Position *</Label>
+								<Input id='position' required />
+							</div>
+						</div>
+						<div>
+							<Label> Location</Label>
+							<Input id='location' />
+						</div>
+						<div>
+							<Label> Salary</Label>
+							<Input id='salary' />
+						</div>
+					</div>
+				</form>
+			</DialogContent>
+		</Dialog>
+	)
 }
 
 export default CreateJobApplicationDialog
